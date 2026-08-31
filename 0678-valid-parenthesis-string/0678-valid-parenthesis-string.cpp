5275ac54413135f194c7cb5fbff2a30270a256fd
pp8478586-sketch <pp8478586@gmail.com>
2026-09-01 00:34:35 +0530

    Time: 0 ms (100%), Space: 8.1 MB (74.71%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int min=0;
-        int max=0;
+        int minBalance=0;
+        int maxBalance=0;
         for(int i=0; i<s.size();i++){
             if(s[i]=='('){
-                min+=1;
-                max+=1;
+                minBalance+=1;
+                maxBalance+=1;
             }
             else if(s[i]==')'){
-                min-=1;
-                max-=1;
+                minBalance-=1;
+                maxBalance-=1;
             }
             else{
-                min-=1;
-                max+=1;
+                minBalance-=1;
+                maxBalance+=1;
             }
-            if(min<0){
-                min=0;
+            if(minBalance<0){
+                minBalance=0;
             }
-            if(max<0){
+            if(maxBalance<0){
                 return false;
             }
         }
-        if(min==0){
+        if(minBalance==0){
             return true;
         }
         return false;
