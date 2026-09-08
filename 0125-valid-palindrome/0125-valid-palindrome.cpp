7ad219440fb9cdf77caab35ce99e477b48d270eb
pp8478586-sketch <pp8478586@gmail.com>
2026-09-08 16:54:58 +0530

    Time: 0 ms (100%), Space: 9.9 MB (77.01%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string a;
-        for(char c:s){
-            if(isalnum(c)){
-                c=tolower(c);
-                a.push_back(c);
-            }
+      int n=s.size();
+      int begin=0;
+      int end=n-1;
+      while(begin<end){
+        while(begin<end&&!isalnum(s[begin])){
+            begin++;
         }
-        int n=a.size();
-        for(int i=0;i<n/2;i++){
-            if(a[i]!=a[n-i-1]){
-                return false;
-            }
+        while(begin<end&&!isalnum(s[end])){
+            end--;
         }
-        return true;
+        if(begin>=end) break;
+        char first=tolower(s[begin]);
+        char second=tolower(s[end]);
+        if(first!=second)return false;
+        begin++;
+        end--;
+        
+      }
+      return true;
     }
 };
