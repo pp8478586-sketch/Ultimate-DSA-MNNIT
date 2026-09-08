98389052a61849d2bcc68e86e112d87d0710f0d2
pp8478586-sketch <pp8478586@gmail.com>
2026-09-08 23:42:17 +0530

    Time: 0 ms (100%), Space: 11.4 MB (19.53%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,76 +1,19 @@
 class Solution {
 public:
-    bool sorted(vector<int>arr) {
-        
-        for(int i=0;i<arr.size()-1;i++){
-            if(arr[i]>arr[i+1]){
-               return false; 
-                
-            }
-            
-                 }
-    return true;
-    }
-    // bool checksort(vector<int>&arr){
-    //     if(sorted(arr)){
-    //         return true;
-    //     }
-    //     else{
-    //     vector<int>v=arr;
-        
-    //     for(int j=0;j<arr.size();j++){
-    //         int temp=arr[j];
-    //         int n=arr.size();
-    //         for(int i=0;i<arr.size();i++){
-    //             arr[i]=arr[i+1];
-    //             arr[n-1] =temp;
-    //             }
-    //         if(sorted(arr)){
-    //             return true;
-    //         }
-
-            
-    //     }
-    //     }
-    //     return false;
-    
-        
-    // }
-    void rotate(vector<int>&nums){
-            int temp=nums[0];
-            int n=nums.size();
-            for(int i=0;i<nums.size()-1;i++){
-                nums[i]=nums[i+1];
-                
-                }
-                nums[n-1] =temp;
-
-    }
-    
-
     bool check(vector<int>& nums) {
-
-        
-     if(sorted(nums)){
-            return true;
-        }
-        else{
-        vector<int>temp=nums;
-        
-        
-        for(int j=0;j<nums.size();j++){
-            rotate(temp);
-            
-            if(sorted(temp)){
-                return true;
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            if(nums[i+1]<nums[i]){
+                count++;
+                if(count>1)return false;
             }
-
-            
         }
+        if(count==1){
+            if(nums[n-1]<=nums[0])return true;
+            return false;
         }
-        return false;
-    
+        return true;
         
-        
-    } 
+    }
 };
