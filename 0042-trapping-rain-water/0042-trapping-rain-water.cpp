b84c9509e1c2ec106efdb899c866f2d95e1265d5
pp8478586-sketch <pp8478586@gmail.com>
2026-09-14 01:19:49 +0530

    Time: 4 ms (8.53%), Space: 27.3 MB (16.36%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-       int water=0;
-       stack<int>st;
-        for(int i=0;i<height.size();i++){
-            while(!st.empty()&&height[st.top()]<height[i]){
-                int currHeight=height[st.top()];
-                st.pop();
-                int left=-1;
-                if(!st.empty()){
-                    left=st.top();
-                }
-                if(left!=-1){
-                    water+=(i-left-1)*(min(height[left],height[i])-currHeight);
-                }
+        int n=height.size();
+        vector<int>prefixMax(n,-1);
+        vector<int>suffixMax(n,-1);
+        int leftMax=0;
+         int rightMax=n-1;
+        for(int i=0;i<n;i++){
+            
+            if(height[i]>height[leftMax]){
+                prefixMax[i]=i;
+                leftMax=i;
             }
-            st.push(i);
+            else{
+                prefixMax[i]=leftMax;
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+           
+            if(height[i]>height[rightMax]){
+                suffixMax[i]=i;
+                rightMax=i;
+            }
+            else{
+                suffixMax[i]=rightMax;
+            }
+        }
+        int water=0;
+        for(int i=0;i<n;i++){
+            if(height[prefixMax[i]]>height[i]&&height[suffixMax[i]]>height[i]){
+                 water+=(min(height[prefixMax[i]],height[suffixMax[i]])-height[i]);
+            }
+           
         }
         return water;
         
