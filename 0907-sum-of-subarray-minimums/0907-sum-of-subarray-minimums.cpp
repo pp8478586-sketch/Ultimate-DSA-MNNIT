7fe10c8d31d207d332d16792870d8bd65c9c0fec
pp8478586-sketch <pp8478586@gmail.com>
2026-09-15 23:11:19 +0530

    Time: 35 ms (97.79%), Space: 170.5 MB (97.99%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -2,36 +2,20 @@ class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
         int n=arr.size();
-        vector<int>prevSmaller(n,-1);
-        vector<int>nextSmaller(n,n);
-        stack<int>st1;
+        stack<int>st;
         long long mod=1e9+7;
         long long  ans=0;
-        for(int i=0;i<n;i++){
-            while(!st1.empty()&&arr[st1.top()]>=arr[i]){
-                nextSmaller[st1.top()]=i;
-                st1.pop();
+        for(int i=0;i<=n;i++){
+            while(!st.empty()&&(i==n||arr[st.top()]>=arr[i])){
+                int mid=st.top();
+                st.pop();
+                int right=i;
+                int left=st.empty()? -1:st.top();
+                ans=ans+1LL*arr[mid]*(mid-left)*(i-mid);
             }
-            st1.push(i);
+            st.push(i);
         }
-        while(!st1.empty()){
-            st1.pop();
-        }
-        for(int i=n-1;i>=0;i--){
-            while(!st1.empty()&&arr[st1.top()]>arr[i]){
-                prevSmaller[st1.top()]=i;
-                st1.pop();
-            }
-            st1.push(i);
-        }
-        for(int i=0;i<n;i++){
-            long long  noOfSubArrays=1LL*(i-prevSmaller[i])*(nextSmaller[i]-i);
-            ans=(ans+1LL*arr[i]*noOfSubArrays)%mod;
-        }
-        return ans;
+        return ans%mod;
 
-
-
-        
     }
 };
