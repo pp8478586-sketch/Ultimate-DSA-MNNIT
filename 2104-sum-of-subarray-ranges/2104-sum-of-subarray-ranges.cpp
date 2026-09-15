f200587d7e5274c469a6e1370999380fe11070aa
pp8478586-sketch <pp8478586@gmail.com>
2026-09-15 23:37:03 +0530

    Time: 3 ms (74.05%), Space: 14 MB (79.93%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.cpp
@@ -2,57 +2,31 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n=nums.size();
-        vector<int>prevSmaller(n,-1);
-        vector<int>nextSmaller(n,n);
-        vector<int>prevGreater(n,-1);
-        vector<int>nextGreater(n,n);
+        long long ans=0;
         stack<int>st;
-        long long minSum=0;
-        long long maxSum=0;
-        for(int i=0;i<n;i++){
-            while(!st.empty()&&nums[st.top()]>=nums[i]){
-                nextSmaller[st.top()]=i;
+        for(int i=0;i<=n;i++){
+            while(!st.empty()&&(i==n||nums[st.top()]>=nums[i])){
+                int mid=st.top();
                 st.pop();
+                int left=st.empty()?-1:st.top();
+                int right=i;
+                ans=ans-1LL*nums[mid]*(mid-left)*(right-mid);
             }
             st.push(i);
         }
         while(!st.empty()){
             st.pop();
         }
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty()&&nums[st.top()]>nums[i]){
-                prevSmaller[st.top()]=i;
+        for(int i=0;i<=n;i++){
+            while(!st.empty()&&(i==n||nums[st.top()]<=nums[i])){
+                int mid=st.top();
                 st.pop();
+                int right=i;
+                int left=st.empty()?-1:st.top();
+                ans=ans+1LL*nums[mid]*(mid-left)*(right-mid);
             }
             st.push(i);
         }
-        while(!st.empty()){
-            st.pop();
-        }
-        for(int i=0;i<n;i++){
-            minSum=minSum+1LL*nums[i]*(i-prevSmaller[i])*(nextSmaller[i]-i);
-        }
-        for(int i=0;i<n;i++){
-            while(!st.empty()&&nums[st.top()]<=nums[i]){
-                nextGreater[st.top()]=i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        while(!st.empty()){
-            st.pop();
-        }
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty()&&nums[st.top()]<nums[i]){
-                prevGreater[st.top()]=i;
-                st.pop();
-            }
-            st.push(i);
-        }
-        for(int i=0;i<n;i++){
-            maxSum=maxSum+1LL*nums[i]*(i-prevGreater[i])*(nextGreater[i]-i);
-        }
-        return maxSum-minSum;
-
+        return ans;
     }
 };
