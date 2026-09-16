92855697b5902b97fca22926868f128a865ad8a3
pp8478586-sketch <pp8478586@gmail.com>
2026-09-17 01:43:07 +0530

    Time: 0 ms (100%), Space: 71.9 MB (14.88%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int maxSum=INT_MIN;
+        for(int num:nums){
+            sum+=num;
+            maxSum=max(sum,maxSum);
+            if(sum<0){
+                sum=0;
+            } 
+        }
+        return maxSum;
+    }
+};
