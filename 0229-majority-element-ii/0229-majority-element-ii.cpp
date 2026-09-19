52e695c0579c8212e95160de4861612380d4a73f
pp8478586-sketch <pp8478586@gmail.com>
2026-09-19 15:23:29 +0530

    Time: 0 ms (100%), Space: 23.8 MB (73.6%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,46 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        if(nums.size()==1)return nums;
         vector<int>ans;
+        int cad1=nums[0];
+        int cad2=nums[1];
+        int count1=0;
+        int count2=0;
         int n=nums.size();
-        int i=0;
-        while(i<n){
-            int current=nums[i];
-            int count=0;
-            while(i<n&&current==nums[i]){
-                count++;
-                i++;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==cad1){
+                count1++;
             }
-            if(count>n/3){
-                ans.push_back(current);
+            else if(nums[i]==cad2){
+                count2++;
+            }
+            else if(count1==0){
+                cad1=nums[i];
+                count1=1;
+            }
+            else if(count2==0){
+                cad2=nums[i];
+                count2=1;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+
+        }
+        int freq1=0;
+        int freq2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==cad1){
+                freq1++;
+            }
+            else if(nums[i]==cad2){
+                freq2++;
             }
         }
+        if(freq1>n/3)ans.push_back(cad1);
+        if(freq2>n/3)ans.push_back(cad2);
         return ans;
     }
 };
