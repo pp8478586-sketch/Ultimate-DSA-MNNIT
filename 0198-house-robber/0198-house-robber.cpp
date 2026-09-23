433c8a64714ce773321910196ba53d3c7b0ee344
pp8478586-sketch <pp8478586@gmail.com>
2026-09-24 03:16:36 +0530

    Time: 0 ms (100%), Space: 10.1 MB (79.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)return nums[0];
-        if(n==2)return max(nums[0],nums[1]);
-        int first=nums[0];
-        int second=nums[1];
-        int third=nums[2]+nums[0];
-        int maxSum=max(first,second);
-            maxSum=max(maxSum,third);
-            
-        for(int i=3;i<n;i++){
-            if(nums[i]>=0){
-                int sum1=first+nums[i];
-                int sum2=second+nums[i];
-                int maxi=max(sum1,sum2);
-                maxSum=max(maxSum,maxi);
-                first=second;
-                second=third;
-                third=maxi;
-            }
-            
-            
+        int n = nums.size();
+
+        if (n == 1) return nums[0];
+
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            int curr = max(prev1, prev2 + nums[i]);
+
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return maxSum;
+
+        return prev1;
     }
 };
