4b822e2891ad1654f43d1466af0bd55915f821ea
pp8478586-sketch <pp8478586@gmail.com>
2026-09-25 03:49:09 +0530

    Time: 0 ms (100%), Space: 8.2 MB (87.09%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[0][0]=1;
+        vector<int>dp(n,0);
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(j-1>=0)dp[i][j]+=dp[i][j-1];
-                if(i-1>=0)dp[i][j]+=dp[i-1][j];
+            dp[0]=1;
+            for(int j=1;j<n;j++){
+                int temp=dp[j];
+                dp[j]=dp[j-1]+temp;
             }
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
